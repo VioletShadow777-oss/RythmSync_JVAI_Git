1dda1dbb3d009f2deb794764a01d51f70fbba6a5
Towhidul-Islam5797 <81187612+Towhidul-Islam5797@users.noreply.github.com>
2026-03-06 12:58:17 +0600

    Pause Panel Activated

--- Assets/_Scripts/UI/UIManager.cs
+++ Assets/_Scripts/UI/UIManager.cs
@@ -4,18 +4,27 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
 
-    [SerializeField] private GameObject mainMenuPanel;
-    [SerializeField] private GameObject hudPanel;
-    [SerializeField] private GameObject countdownPanel;
-    [SerializeField] private GameObject pausePanel;
-    [SerializeField] private GameObject resultsPanel;
+    public GameObject mainMenuPanel;
+    public GameObject hudPanel;
+    public GameObject countdownPanel;
+    public GameObject pausePanel;
+    public GameObject resultsPanel;
 
+    [Header("Settings Panel")]
+    public GameObject settingsPanel;
+    private bool isSettingsActive;
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
         else
             Destroy(gameObject);
+    }
+    private void Start()
+    {
+        isSettingsActive = false; // Initialize settings panel state
+        settingsPanel.SetActive(false); // Ensure it's hidden at start
+
     }
 
     private void OnEnable()
@@ -49,13 +58,32 @@ public class UIManager : MonoBehaviour
             case GameState.Playing:
                 hudPanel.SetActive(true);
                 break;
-            case GameState.Paused:
-                hudPanel.SetActive(true);
-                pausePanel.SetActive(true);
-                break;
             case GameState.Results:
                 resultsPanel.SetActive(true);
                 break;
         }
     }
+
+    // this method toggles between acive and disable the settings panel
+
+    public void ToggleSettingsPanel()
+    {
+        if(isSettingsActive == false)
+        {
+            settingsPanel.SetActive(true);
+            isSettingsActive = true;
+
+            //Pause the game when settings panel is active
+            GameManager.Instance.PauseGame();
+        }
+        else
+        {
+            settingsPanel.SetActive(false);
+            isSettingsActive = false;
+
+            //Resume the game when settings panel is closed
+            GameManager.Instance.ResumeGame();
+        }
+    }
+
 }
